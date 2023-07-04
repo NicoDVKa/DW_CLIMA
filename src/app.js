0edb8056ad5471837e51b1f5c6c6a1346da98e2d
NicoHerlan <nicodvkcss@gmail.com>
2023-07-03 23:23:53 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,168 @@
+const { Client } = require('pg');
+const cors = require('cors');
+const express = require('express');
+const { exportClient, Port } = require('./config.js');
+
+const app = express();
+
+app.use(cors());
+
+const client = new Client(exportClient);
+
+client.connect();
+
+
+// !Consulta a todos los datos de la tabla base.
+app.get('/medicion', (req, res) => {
+  client.query('SELECT * FROM bt_medicion', (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error en la consulta');
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+
+// !Consulta por todos los años que existan en la table base.
+app.get('/anio', (req, res) => {
+  client.query(`
+              select m.anio
+              FROM public.bt_medicion m
+              group by m.anio
+              order by m.anio;
+              `, (err,result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error en la consulta');
+    }else{
+      res.json(result.rows);
+    }
+  });
+});
+
+
+// !Consulta por las provincias de la lookup
+app.get('/provincia', (req, res) => {
+
+  client.query(
+               `
+               SELECT p.id, p."desc", p.codigo
+               FROM public.lk_provincia as p 
+               inner join bt_medicion as bm on bm.provincia_id =  p.id 
+               where  not bm.cultivo_id = 0  
+               group  by p.id 
+               order by p.id ;
+               `
+                ,(err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error en la consulta');
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+
+// !Se trae los datos de la temperatura anual de la provincia. Además, tiene un filtro por año.
+app.get('/provincia/temperatura', (req, res) => {
+  let {year}   = req.query;
+  client.query(
+               `SELECT lp.id , lp."desc" , bt.anio,  max(bt.temperatura) as temperatura
+                FROM public.bt_medicion as bt
+                inner join  lk_provincia as lp on lp.id = bt.provincia_id
+                ${year ? 'where bt.anio =' + year : ''}
+                group by lp.id, bt.anio
+                order by lp.id, bt.anio;`
+                ,(err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error en la consulta');
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+
+// !Se trae el nombre de los cultivos que hay en la tabla base. Además, tiene filtro por año. (Sirve para filtrado)
+app.get('/cultivo', (req, res) => {
+  let {year, province} = req.query;
+  client.query(
+               `
+               SELECT c.id, c."desc", c.codigo
+               FROM public.lk_cultivo as c
+               inner join bt_medicion as m on  m.cultivo_id = c.id 
+               where "desc" is not null ${year? 'and m.anio ='+ year : ''} ${province? 'and m.provincia_id ='+ province : ''} 
+               group by c.id 
+               order by c.id;
+               `
+                ,(err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error en la consulta');
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+
+// !Se trae las mediciones de los cultivos que hay en la tabla base. Además, tiene filtro por año e id del cultivo.
+app.get('/cultivo/medicion/:year', (req, res) => {
+  let {province, crop} = req.query;
+  let {year} = req.params;
+  client.query(
+               `
+               SELECT lc.id, lc."desc" as cultivo_desc,lp.desc as provincia_desc, avg(bt.rendimiento) as rendimiento , avg(bt.produccion) as produccion, avg(bt. sup_sembrada) as sup_sembrada , avg(bt.sup_cosechada) as sup_cosechada
+               FROM public.bt_medicion as bt
+               inner join  lk_cultivo as lc on lc.id = bt.cultivo_id
+               inner join lk_provincia as lp on lp.id  = bt.provincia_id  
+                where  not lc.id = 0  and  bt.anio=${year} ${province? 'AND lp.id= ' + province :''} ${crop? 'AND lc.id=' + crop : ''}
+                group by lc.id, bt.anio, lp.desc
+                order by lc.id, bt.anio
+               `
+                ,(err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error en la consulta');
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+
+
+
+app.get('/cultivo/medicion', (req, res) => {
+  let {province,crop} = req.query;
+  client.query(
+               `
+               SELECT lc.id, lc."desc" as cultivo_desc, ${province? 'lp.desc as provincia_desc ,': ''} bt.anio,  avg(bt.rendimiento) as rendimiento , avg(bt.produccion) as produccion, avg(bt. sup_sembrada) as sup_sembrada , avg(bt.sup_cosechada) as sup_cosechada
+               FROM public.bt_medicion as bt
+               inner join  lk_cultivo as lc on lc.id = bt.cultivo_id
+               inner join lk_provincia as lp on lp.id  = bt.provincia_id  
+              where  not lc.id = 0  ${province? ' And lp.id ='+ province : ''} ${crop? ' And lc.id ='+ crop : ''}
+                group by lc.id, bt.anio   ${province? ' , lp.id': ''}
+                order by lc.id, bt.anio ${province? ' , lp.id': ''}
+               `
+                ,(err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error en la consulta');
+    } else {
+      res.json(result.rows);
+    }
+  });
+});
+
+
+
+
+app.listen(Port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${Port}`);
+});
+
---
+++ src/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    exportClient: {
+      user: 'postgres',
+      host: 'localhost',
+      database: 'dw_clima',
+      password: '1234',
+      port: 5432,
+    },
+    Port : process.env.PORT || 3000
+  };
+  
+
+// !Datos de la base de datos local
+
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'dw_clima',
+//   password: '1234',
+//   port: 5432,
+
+
+// !Datos de la base de datos hosteada
+
+//      user: 'postgres',
+//      host: 'containers-us-west-88.railway.app',
+//      database: 'railway',
+//      password: 'YT6ZwWghepVjPLs48mCb',
+//      port: 7858,

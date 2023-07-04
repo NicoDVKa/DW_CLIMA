319af711c28c5b9ed92647b72e1d79188c4a4866
NicoHerlan <nicodvkcss@gmail.com>
2023-07-03 23:25:56 -0300

    add config bd railway

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     exportClient: {
       user: 'postgres',
-      host: 'localhost',
-      database: 'dw_clima',
-      password: '1234',
-      port: 5432,
+        host: 'containers-us-west-88.railway.app',
+        database: 'railway',
+        password: 'YT6ZwWghepVjPLs48mCb',
+        port: 7858,
     },
     Port : process.env.PORT || 3000
   };
@@ -21,8 +21,8 @@ module.exports = {
 
 // !Datos de la base de datos hosteada
 
-//      user: 'postgres',
-//      host: 'containers-us-west-88.railway.app',
-//      database: 'railway',
-//      password: 'YT6ZwWghepVjPLs48mCb',
-//      port: 7858,
+//  user: 'postgres',
+//  host: 'containers-us-west-88.railway.app',
+//  database: 'railway',
+//  password: 'YT6ZwWghepVjPLs48mCb',
+//  port: 7858,
